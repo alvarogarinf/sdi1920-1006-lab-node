807b2bf495b47cc1eb362ac93fa64b5206ac875c
Álvaro <alvarogarciainfante@gmail.com>
2020-03-11 19:55:10 +0100

    SDI-NodeJS-.3,4,5 Express, depuración y varibles de entorno

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var express = require('express');
 var app = express();
 // Variables
 app.set('port', 8081);
-app.get('/usuarios', function(req, res) {
-    res.send('ver usuarios');
-})
-app.get('/canciones', function(req, res) {
-    res.send('ver canciones');
-})
+
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 // lanzar el servidor
 app.listen(app.get('port'), function() {
     console.log("Servidor activo");
-})
+});
+
+app.get('/canciones/:id', function(req, res) {
+    var respuesta = 'id: ' + req.params.id;
+    res.send(respuesta);
+});
+app.get('/canciones/:genero/:id', function(req, res) {
+    var respuesta = 'id: ' + req.params.id + '<br>'
+        + 'Género: ' + req.params.genero;
+    res.send(respuesta);
+});

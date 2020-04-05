db6383dd3c7c1a101097313f14f7530c4b29b151
Álvaro <alvarogarciainfante@gmail.com>
2020-04-05 13:43:07 +0200

    SDI-NodeJS-10.5- SW Consumir SW desde NodeJS

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -62,22 +62,26 @@ module.exports = function (app, swig, gestorBD) {
                         if (compras == null) {
                             res.send("Error al listar ");
                         } else {
-                            let cancionesCompradasIds = [];
-                            for (i = 0; i < compras.length; i++) {
-                                if (req.params.id == compras[i].cancionId) {
-                                    cancionesCompradasIds.push(compras[i].cancionId);
-
+                            var configuracion = {
+                                url: "https://api.exchangeratesapi.io/latest?base=EUR",
+                                method: "get",
+                                headers: {
+                                    "token": "ejemplo",
                                 }
                             }
-
-                            let respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    comprada: cancionesCompradasIds.length,
-                                    usuario: req.session.usuario,
-                                    comentario: comentarios,
-                                    cancion: canciones[0]
-                                });
-                            res.send(respuesta);
+                            var rest = app.get("rest");
+                            rest(configuracion, function (error, response, body) {
+                                console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                                var objetoRespuesta = JSON.parse(body);
+                                var cambioUSD = objetoRespuesta.rates.USD;
+                                // nuevo campo "usd"
+                                canciones[0].usd = cambioUSD * canciones[0].precio;
+                                var respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        cancion: canciones[0]
+                                    });
+                                res.send(respuesta);
+                            })
                         }
                     });
                 });
